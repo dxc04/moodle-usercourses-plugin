b4e108770cdf094c2f63e5aefe1d653c9f5d2c4c
Dixie Philamerah Atay <dixie.atay@gmail.com>
2021-03-17 19:53:44 +0800

    Added version.php

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+$plugin->version  = 2021031700;   
+$plugin->requires = 2020110900;
+$plugin->component = 'local_usercourses';
+$plugin->cron     = 0;
+$plugin->release = '1.0 (Build: 2021031700)';
+$plugin->maturity = MATURITY_STABLE;
